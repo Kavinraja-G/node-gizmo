4f006d209e0ffb6bf9c27b1ccf827e6ed115e935
Kavin <kavinraja.ganesan@gmail.com>
2023-11-04 19:37:55 +0530

    feat: Support Karpenter nodes and nodepoolInfo grouping

--- pkg/constants.go
+++ pkg/constants.go
@@ -2,9 +2,10 @@ package pkg
 
 // required constants used across the CLI commands (mostly well-known labels, annotations etc.,)
 const (
-	AwsNodepoolLabel = "eks.amazonaws.com/nodegroup"
-	GkeNodepoolLabel = "cloud.google.com/gke-nodepool"
-	AksNodepoolLabel = "kubernetes.azure.com/agentpool"
+	AwsNodepoolLabel  = "eks.amazonaws.com/nodegroup"
+	GkeNodepoolLabel  = "cloud.google.com/gke-nodepool"
+	AksNodepoolLabel  = "kubernetes.azure.com/agentpool"
+	KarpenterNodepool = "karpenter.sh/provisioner-name"
 
 	NodeInstanceTypeLabel = "node.kubernetes.io/instance-type"
 	TopologyRegionLabel   = "topology.kubernetes.io/region"
